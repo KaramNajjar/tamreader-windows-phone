0dba77a0d08e31dd896a3c4707ace9a2eac43537
Karam <karam.0696@gmail.com>
2017-01-21 17:26:38 +0200

    tamreader windows phone side

---
+++ tamreader/Logic/Sign.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamreader.Logic
+{
+   public class Sign
+    {
+        private String name;
+        private int obeyTimes;
+        private int disobeyTimes;
+
+        public Sign() {
+
+
+        }
+        public Sign(Sign sign)
+        {
+            this.name = sign.Name;
+            this.obeyTimes = sign.obeyTimes;
+            this.disobeyTimes = sign.DisobeyTimes;
+        }
+
+        /// <summary>
+        /// The Sign's name.
+        /// </summary>
+        public String Name {
+            get { return name; }
+            set { name = value; }
+           }
+
+        /// <summary>
+        /// The Sign's Obedience times. 
+        /// </summary>
+        public int ObeyTimes
+        {
+            get { return obeyTimes; }
+            set { obeyTimes = value; }
+        }
+
+
+        /// <summary>
+        /// The Sign's DisObedience times. 
+        /// </summary>
+        public int DisobeyTimes
+        {
+            get { return disobeyTimes; }
+            set { disobeyTimes = value; }
+        }
+    }
+}
